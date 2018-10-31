21e8bb38dae0965baf9cef03b5d80afe200aac89
lightjames <lightjameslyy@gmail.com>
2018-10-31 09:24:02 +0800

    update

---
+++ python-basics/02-python-basics/01_python_basics/lt_06_person.py
@@ -0,0 +1,21 @@
+"""
+姓名：小明
+年龄：18
+性别：男
+身高：1.75m
+体重：75Kg
+
+"""
+
+# python自动推导类型，不需要指定变量类型
+name = "小明"  # str
+
+age = 18  # int
+
+isMale = True  # bool
+
+height = 1.75  # float
+
+weight = 75.0  # float
+
+print(name)
